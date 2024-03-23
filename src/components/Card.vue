599f7f58cff528ad696d9bf8407a35e358625c68
CesarTracana <cedtg12@gmail.com>
2024-03-23 11:52:36 -0400

    Rick y Morty

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue';
+import Card  from ".../components/Card.vue"
+
+    const props = ({
+        character: []
+    })
+    console.log(character.value.name)
+</script>
+
+<template>
+    <div>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h3>nombre</h3>
+        </div>
+    </div>
+</template>
